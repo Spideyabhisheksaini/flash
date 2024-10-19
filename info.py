6594635157680526a8841e33c2c87170b0a5e314
Spideyabhisheksaini <sainiabhi310@gmail.com>
2024-10-19 18:04:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://Jarvis:MBZPyEsIMs2DMS0y@cluster0.qdaityo.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://flash:dbflashpoint@cluster0.pnsxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Flash: Flash@flash.w4skg.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Flashdb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
